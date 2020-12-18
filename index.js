efa7c66d5e6014c75494be59a3e83368b04d071f
Christian Glahn <cpglahn@gmail.com>
2020-12-19 00:27:58 +0100

    info on protected paths

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ async function action() {
 
     const protected_paths = protected_defaults.concat(protected_array);
 
+    core.info(JSON.stringify(protected_paths));
+
     // check normal protected files
     const hold_protected = files
         .map((file) => protected_paths

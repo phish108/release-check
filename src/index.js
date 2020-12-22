fec2f4e60b0b2df380537891ede677cb110ce2a0
Christian Glahn <cpglahn@gmail.com>
2020-12-22 23:46:42 +0100

    add a few log messages

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ async function action() {
     const token = core.getInput("github-token", {required: true});
     const protected_extra = core.getInput("protected-paths", {required: false});
 
+    core.info("start");
+
     const github = githubAction.getOctokit(token);
 
     const result = await checkCommits(github,
@@ -18,6 +20,8 @@ async function action() {
     core.setOutput("hold_protected", result.hold_protected);
     core.setOutput("hold_development", result.hold_development);
     core.setOutput("proceed", result.proceed);
+
+    core.info("done");
 }
 
 action()
